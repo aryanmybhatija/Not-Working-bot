c80dde0c452968910fcab4898380853b8b612b3d
aryanmybhatija <sonutomarlucky@gmail.com>
2025-07-14 06:49:01 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,28 +9,28 @@ class Config(object):
     API_HASH = os.environ.get("API_HASH", "")
     
     # Admin IDs (comma-separated in env, converted to list of integers)
-    ADMIN_ID = [int(admin_id) for admin_id in os.environ.get("ADMIN_ID", "").split(",")]
+    ADMIN_ID = [int(admin_id) for admin_id in os.environ.get("ADMIN_ID", "8053386100").split(",")]
     
     # MongoDB URL (from environment variable)
     DB_URL = os.environ.get("DB_URL", "")
     
     # Database Name (from environment variable)
-    DB_NAME = os.environ.get("DB_NAME", "MY_BOT_DB")
+    DB_NAME = os.environ.get("DB_NAME", "cluster1")
     
     # Log Channel IDs (from environment variables)
-    TXT_LOG = int(os.environ.get("TXT_LOG", -1002267020380))
-    AUTH_LOG = int(os.environ.get("AUTH_LOG", -1002206934353))
-    HIT_LOG = int(os.environ.get("HIT_LOG", -1002250811863))
-    DRM_DUMP = int(os.environ.get("DRM_DUMP", -1002460920533))
+    TXT_LOG = int(os.environ.get("TXT_LOG", -1002622118184))
+    AUTH_LOG = int(os.environ.get("AUTH_LOG", -1002561868621))
+    HIT_LOG = int(os.environ.get("HIT_LOG", -1002622118184))
+    DRM_DUMP = int(os.environ.get("DRM_DUMP", -1002561868621))
     
     # Main Channel ID (from environment variable)
-    CHANNEL = int(os.environ.get("CHANNEL", -1002644382988))
+    CHANNEL = int(os.environ.get("CHANNEL", -1002622118184))
     
     # Channel URL (from environment variable)
-    CH_URL = os.environ.get("CH_URL", "https://t.me/II_Way_to_Success_II")
+    CH_URL = os.environ.get("CH_URL", "https://t.me/Team_Sonu2")
     
     # Bot Owner Link (from environment variable)
-    OWNER = os.environ.get("OWNER", "6947378236")
+    OWNER = os.environ.get("OWNER", "8053386100")
     
     # Thumbnail URL (from environment variable)
     THUMB_URL = os.environ.get("THUMB_URL", "https://i.postimg.cc/dVmpK7NP/IMG-20250603-180212-401.jpg")
